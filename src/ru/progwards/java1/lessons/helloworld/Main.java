839ad0f4e4fa1b766e39dba47311d8e09ebf91a8
Visual Studio 2015 <fiv15184@ploae.com>
2019-04-11 12:41:43 +0300

    HelloWorld start

---
+++ src/ru/progwards/java1/lessons/helloworld/Main.java
@@ -0,0 +1,21 @@
+package ru.progwards.java1.lessons.helloworld;
+
+public class Main {
+
+    static void println(String message) {
+        System.out.println(message);
+    }
+
+    static int addition(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        String hello = "Hello World!";
+        println(hello);
+
+        int result = addition(7, 18);
+        System.out.print("Сумма равна: ");
+        System.out.println(result);
+    }
+}
